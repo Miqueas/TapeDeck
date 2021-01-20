20aa3ab642eb1bb34cd46d3f9bd7a675d7d8dbeb
Miqueas Martinez <miqueas2020@yahoo.com>
2021-01-20 17:57:34 -0500

    MoonplayerInfo ahora es un GtkEventBox.
    - MediaArtEv no existe (ya que ahora es la clase en sí)
    - MediaArtImg ahora es solo Img
    - Comentado el código
    - La propiedad img ahora tiene el id 1 (title es 2 y artist es 3)
    - Añadido override para la señal "button-press-event"

--- Source/Info.lua
+++ Source/Info.lua
@@ -1,69 +1,82 @@
-Moonplayer:class("Info", Hdy.Clamp)
+Moonplayer:class("Info", Gtk.EventBox)
 
---[[ TODO Properties:
-
-img?
-img_size?
-
-]]
-
+-- Class constructor
 function Moonplayer.Info:_class_init(klass)
   klass:set_template_from_resource("/com/github/M1que4s/Moonplayer/Templates/Info.ui")
+  klass:bind_template_child_full("Img", true, 0)
   klass:bind_template_child_full("Title", true, 0)
   klass:bind_template_child_full("Artist", true, 0)
-  klass:bind_template_child_full("MediaArtEv", true, 0)
-  klass:bind_template_child_full("MediaArtImg", true, 0)
 
   function klass:set_property(id, value, pspec)
+    -- Moonplayer.Info.img property
     if id == 1 then
-      self.priv.title = value:get_string()
-    elseif id == 2 then
-      self.priv.artist = value:get_string()
-    elseif id == 3 then
       self.priv.img = value:get_object()
+
+    -- Moonplayer.Info.title property
+    elseif id == 2 then
+      self.priv.title = value:get_string()
+
+    -- Moonplayer.Info.artist property
+    elseif id == 3 then
+      self.priv.artist = value:get_string()
+
+    -- Invalid property
     else
       GObject.OBJECT_WARN_INVALID_PROPERTY_ID(self, id, pspec)
     end
   end
 
   function klass:get_property(id, value, pspec)
+    -- Moonplayer.Info.img property
     if id == 1 then
-      value:set_string(self.priv.title)
-    elseif id == 2 then
-      value:set_string(self.priv.artist)
-    elseif id == 3 then
       value:set_object(self.priv.img)
+
+    -- Moonplayer.Info.title property
+    elseif id == 2 then
+      value:set_string(self.priv.title)
+
+    -- Moonplayer.Info.artist property
+    elseif id == 3 then
+      value:set_string(self.priv.artist)
+
+    -- Invalid property
     else
       GObject.OBJECT_WARN_INVALID_PROPERTY_ID(self, id, pspec)
     end
   end
 
-  klass:install_property(1, GObject.ParamSpecString(
-      "title", "Title", "Media title", "Title",
+  klass:install_property(1, GObject.ParamSpecObject(
+      "img", "Image", "Media album artwork", GdkPixbuf.Pixbuf,
       { "READWRITE", "CONSTRUCT" }
     )
   )
 
   klass:install_property(2, GObject.ParamSpecString(
-      "artist", "Artist", "Media artist name", "Artist",
+      "title", "Title", "Media title", "Moonplayer",
       { "READWRITE", "CONSTRUCT" }
     )
   )
 
-  klass:install_property(3, GObject.ParamSpecObject(
-      "img", "Image", "Media album artwork", GdkPixbuf.Pixbuf,
+  klass:install_property(3, GObject.ParamSpecString(
+      "artist", "Artist", "Media artist name", "Astronauts music player",
       { "READWRITE", "CONSTRUCT" }
     )
   )
 end
 
+-- Instance constructor
 function Moonplayer.Info:_init()
   self:init_template()
 
+  self.priv.Img = self:get_template_child(Moonplayer.Info, "Img")
   self.priv.Title = self:get_template_child(Moonplayer.Info, "Title")
   self.priv.Artist = self:get_template_child(Moonplayer.Info, "Artist")
-  self.priv.MediaArtEv = self:get_template_child(Moonplayer.Info, "MediaArtEv")
-  self.priv.MediaArtImg = self:get_template_child(Moonplayer.Info, "MediaArtImg")
+
+  GObject.Binding.bind_property(
+    self, "img",
+    self.priv.Img, "pixbuf",
+    GObject.BindingFlags.DEFAULT
+  )
 
   GObject.Binding.bind_property(
     self, "title",
@@ -76,10 +89,8 @@ function Moonplayer.Info:_init()
     self.priv.Artist, "label",
     GObject.BindingFlags.DEFAULT
   )
+end
 
-  GObject.Binding.bind_property(
-    self, "img",
-    self.priv.MediaArtImg, "pixbuf",
-    GObject.BindingFlags.DEFAULT
-  )
+-- TODO: do something when user clicks the info widget
+function Moonplayer.Info:do_button_press_event()
 end
