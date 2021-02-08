4da2aeac56338e742e636ed61576b068b382c3a6
Miqueas Martinez <miqueas2020@yahoo.com>
2021-02-08 00:53:14 -0500

    Implementadas corutinas para lectura "asíncrona" de archivos de usuario

--- Source/App.lua
+++ Source/App.lua
@@ -1,25 +1,8 @@
 Moonplayer:class("App", Gtk.Application)
 
-function Moonplayer.App:_init()
-  self.application_id = "com.github.M1que4s.Moonplayer"
-end
+local flow
 
-function Moonplayer.App:do_startup()
-  self._parent.do_startup(self)
-  Hdy.init()
-  Gst.init()
-
-  -- Creates the window
-  Moonplayer.Window({
-    application = self,
-    id          = "Window"
-  })
-end
-
-function Moonplayer.App:do_activate()
-  self.active_window:present()
-
-  local flow = self.active_window.LocalView.FlowBox
+local function ScanLocalFiles(flow)
   local path = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_MUSIC)
   local folder = Gio.File.new_for_path(path)
 
@@ -41,19 +24,51 @@ function Moonplayer.App:do_activate()
           if tags then
             item.title  = tags:get_string("title")
             item.artist = tags:get_string("artist")
-            --item:set_artwork()
+            item:set_artwork()
             flow:insert(item, count)
           else
-            item.artist = info:get_name()
+            item.title = info:get_name()
             flow:insert(item, count)
           end
         end
 
         info = files:next_file()
         count = count + 1
+        coroutine.yield()
       end
     end
   else
     Log.warning("Error openning folder '"..path.."'")
   end
+end
+
+local co = coroutine.create(ScanLocalFiles)
+
+local function ScannerWrap()
+  stat = coroutine.resume(co, flow)
+
+  if stat then return true else return false end
+end
+
+function Moonplayer.App:_init()
+  self.application_id = "com.github.M1que4s.Moonplayer"
+end
+
+function Moonplayer.App:do_startup()
+  self._parent.do_startup(self)
+  Hdy.init()
+  Gst.init()
+
+  -- Creates the window
+  Moonplayer.Window({
+    application = self,
+    id          = "Window"
+  })
+end
+
+function Moonplayer.App:do_activate()
+  flow = self.active_window.LocalView.FlowBox
+  GLib.timeout_add(400, 400, ScannerWrap)
+
+  self.active_window:present()
 end
