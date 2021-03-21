0fdd5e8296703c6574a01831d78599fdae1d2c58
Miqueas Martinez <miqueas2020@yahoo.com>
2021-03-21 06:12:51 -0500

    Creado esquema XML de GSetting y creado archivo Source/Database

---
+++ Source/Database.lua
@@ -0,0 +1,13 @@
+Moonplayer:class("Database", GObject.Object)
+
+function Moonplayer.Database:_class_init(Klass)
+end
+
+function Moonplayer.Database:_init()
+end
+
+function Moonplayer.Database:create()
+end
+
+function Moonplayer.Database:update()
+end
