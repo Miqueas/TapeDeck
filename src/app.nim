54218784cfa4ccaaae64cf5f7e767f32be758f14
Miqueas <miqueas2020@yahoo.com>
2023-03-11 00:15:00 -0500

    Moved GUI constructs to the ::startup signal

--- src/app.nim
+++ src/app.nim
@@ -1,13 +1,17 @@
 import gintro/[gtk4, adw, gobject, gio]
-import resources
 
 const appID = "io.github.Miqueas.TapeDeck"
 
-proc onActivate(self: adw.Application) =
+proc onStartup(self: adw.Application) =
   let win = adw.newApplicationWindow(self)
+  win.setDefaultSize(400, 300)
+
+proc onActivate(self: adw.Application) =
+  let win = self.activeWindow()
   win.present()
 
 let app = adw.newApplication(appID, ApplicationFlagsDefaultFlags)
+app.connect("startup", onStartup)
 app.connect("activate", onActivate)
 
 discard app.run()
