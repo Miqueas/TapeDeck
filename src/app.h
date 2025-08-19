c7833a35e67d7ad658caf816cb412ad50c113c8e
Josué Martínez <JosueTech@proton.me>
2025-08-18 19:06:08 -0500

    Minor changes

--- src/app.h
+++ src/app.h
@@ -7,26 +7,6 @@
 #include <glib-object.h>
 #include <glib.h>
 
-#if !defined(__clang__) && defined(__GNUC__) && (__GNUC__ >= 14)
-  #define PUSH_IGNORE_DISCARDS_VOLATILE \
-    _Pragma("GCC diagnostic push") \
-    _Pragma("GCC diagnostic ignored \"-Wdiscarded-qualifiers\"")
-#elif defined(__clang__) && (__clang_major__ >= 16)
-  #define PUSH_IGNORE_DISCARDS_VOLATILE \
-    _Pragma("clang diagnostic push") \
-    _Pragma("clang diagnostic ignored \"-Wincompatible-pointer-types-discards-qualifiers\"")
-#else
-  #define PUSH_IGNORE_DISCARDS_VOLATILE /* nothing */
-#endif
-
-#if !defined(__clang__) && defined(__GNUC__) && (__GNUC__ >= 14)
-  #define POP_IGNORE_DISCARDS_VOLATILE _Pragma("GCC diagnostic pop")
-#elif defined(__clang__) && (__clang_major__ >= 16)
-  #define POP_IGNORE_DISCARDS_VOLATILE _Pragma("clang diagnostic pop")
-#else
-  #define POP_IGNORE_DISCARDS_VOLATILE /* nothing */
-#endif
-
 G_BEGIN_DECLS
 
 #define TPD_TYPE_APP (tpd_app_get_type())
--- src/consts.h
+++ src/consts.h
@@ -1,1 +1,6 @@
+#ifndef __CONSTS_H__
+#define __CONSTS_H__
+
 #define APP_ID "io.github.Miqueas.TapeDeck"
+
+#endif
